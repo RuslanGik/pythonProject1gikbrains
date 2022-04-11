9e961f7e0ebb37afe984299a255ac6ae779e40f6
RuslanGik <englishr@yandex.ru>
2022-04-11 20:04:38 +0300

    Homework 1.1

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран. Запросите у пользователя некоторые числа и
+# строки и сохраните в переменные, затем выведите на экран.
+j = 'Hi, '
+k = 'User!'
+print(j, k)
+number1 = int(input('Enter your favorite number: '))
+number2 = int(input('Enter your least favorite number: '))
+print(f'You have entered {number1} and {number2} ')
+word = input('Please, input any word: ')
+print(f'{word} is very nice!')
